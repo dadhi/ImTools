0115890eec97af818e519bbdf342e5508a20f8c6
Maksim Volkau <max.volkov@gmail.com>
2017-08-11 09:08:12 +0300

    added: Ideal, simple TypeHashCache.. The idea is to make comparable to array access for happy path.

---
+++ src/Playground/TypeHashCache.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Playground
+{
+    public struct TypedValue<T>
+    {
+        public Type Key;
+        public T Value;
+    }
+
+    public static class TypeHashCache
+    {
+        public const int InitialCapacity = 1 << 4;
+
+        public static TypedValue<T>[] Empty<T>(int initialCapacity = InitialCapacity)
+        {
+            return new TypedValue<T>[initialCapacity + (initialCapacity >> 2)];
+        }
+
+        public static T GetOrDefault<T>(this TypedValue<T>[] slots, Type key)
+        {
+            var index = key.GetHashCode() & (slots.Length - 1);
+            var slot = slots[index];
+            if (slot.Key == key)
+                return slot.Value;
+
+            while (true)
+            {
+                slot = slots[++index];
+                if (slot.Key == key)
+                    return slot.Value;
+
+                if (slot.Key == null || //stop on empty slot
+                    index >= slots.Length)
+                    break;
+            }
+
+            return default(T);
+        }
+    }
+}
