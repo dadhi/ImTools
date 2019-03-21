845cd2de704c5eae14d8f98e4fbae6838f9aa048
Maksim Volkau <max.volkov@gmail.com>
2019-03-21 19:12:36 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail #https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+# set -euxo pipefail #https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 #dotnet restore
 
 dotnet test test/ImTools.UnitTests/ImTools.UnitTests.csproj -c:Release -f:netcoreapp2.1 -p:Sign=false;
