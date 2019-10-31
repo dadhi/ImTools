e57d98c2e4733c2145f15ca5498af9de7ab89495
dadhi <max.volkov@gmail.com>
2019-10-31 07:22:07 +0000

    BM to target .NET CORE 3.0 and updated BDN version

--- playground/ImTools.Benchmarks/Program.cs
+++ playground/ImTools.Benchmarks/Program.cs
@@ -10,13 +10,13 @@ namespace ImTools.Benchmarks
             //var x = new ImMapBenchmarks.Populate { Count = 10 };
             //x.ImMapArray_AddOrUpdate();
 
-            //BenchmarkRunner.Run<ImMapBenchmarks.Populate>();
+            BenchmarkRunner.Run<ImMapBenchmarks.Populate>();
             //BenchmarkRunner.Run<ImMapBenchmarks.Lookup>();
 
             //BenchmarkRunner.Run<ImHashMapBenchmarks_StringString.Populate>();
             //BenchmarkRunner.Run<ImHashMapBenchmarks_StringString.Lookup>();
 
-            BenchmarkRunner.Run<ImHashMapBenchmarks.Populate>();
+            //BenchmarkRunner.Run<ImHashMapBenchmarks.Populate>();
             //BenchmarkRunner.Run<ImHashMapBenchmarks.Lookup>();
 
             //BenchmarkRunner.Run<CustomEqualityComparerBenchmarks>();
