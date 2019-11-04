61505f1f691cb2c995f0985354ba42087d4f9d11
dadhi <max.volkov@gmail.com>
2019-11-04 06:29:35 +0000

    fixed the benchmark fo Union

--- playground/ImTools.UnionCsVsFsBenchmarks/Program.fs
+++ playground/ImTools.UnionCsVsFsBenchmarks/Program.fs
@@ -1,31 +1,39 @@
-(*
-|                                     Method |      Mean |     Error |    StdDev | Ratio | RatioSD | Gen 0/1k Op | Gen 1/1k Op | Gen 2/1k Op | Allocated Memory/Op |
-|------------------------------------------- |----------:|----------:|----------:|------:|--------:|------------:|------------:|------------:|--------------------:|
-|                                     FSharp |  11.44 ns | 0.0769 ns | 0.0719 ns |  1.00 |    0.00 |           - |           - |           - |                   - |
-|                   CSharp_named_case_struct |  18.32 ns | 0.0505 ns | 0.0473 ns |  1.60 |    0.01 |           - |           - |           - |                   - |
-| CSharp_named_case_struct_match_I_interface | 304.78 ns | 5.8215 ns | 5.4454 ns | 26.64 |    0.48 |           - |           - |           - |                   - |
-|      CSharp_named_case_struct_Match_method | 134.24 ns | 0.4707 ns | 0.4403 ns | 11.74 |    0.09 |      0.1729 |           - |           - |               816 B |
-*)
-
 open BenchmarkDotNet.Attributes
 open BenchmarkDotNet.Running
 open ImTools
+
+(*
+BenchmarkDotNet=v0.12.0, OS=Windows 10.0.18362
+Intel Core i7-8750H CPU 2.20GHz (Coffee Lake), 1 CPU, 12 logical and 6 physical cores
+.NET Core SDK=3.0.100
+  [Host]     : .NET Core 3.0.0 (CoreCLR 4.700.19.46205, CoreFX 4.700.19.46214), X64 RyuJIT DEBUG
+  DefaultJob : .NET Core 3.0.0 (CoreCLR 4.700.19.46205, CoreFX 4.700.19.46214), X64 RyuJIT
+
+
+|                                     Method |      Mean |    Error |   StdDev | Ratio | RatioSD |  Gen 0 | Gen 1 | Gen 2 | Allocated |
+|------------------------------------------- |----------:|---------:|---------:|------:|--------:|-------:|------:|------:|----------:|
+|                                     FSharp |  12.49 ns | 0.031 ns | 0.029 ns |  1.00 |    0.00 |      - |     - |     - |         - |
+|                   CSharp_named_case_struct |  15.43 ns | 0.085 ns | 0.080 ns |  1.24 |    0.01 |      - |     - |     - |         - |
+|         CSharp_named_case_struct_tag_match |  15.00 ns | 0.162 ns | 0.152 ns |  1.20 |    0.01 |      - |     - |     - |         - |
+| CSharp_named_case_struct_match_I_interface | 248.44 ns | 4.045 ns | 3.784 ns | 19.89 |    0.29 |      - |     - |     - |         - |
+|      CSharp_named_case_struct_Match_method | 181.99 ns | 0.563 ns | 0.527 ns | 14.57 |    0.06 | 0.1733 |     - |     - |     816 B |
+*)
 
 type FlagOrCount = 
     | Flag of bool
     | Count of int
     | Message of string
 
-type Flag1()    = class inherit Case<Flag1, bool>() end
-type Count1()   = class inherit Case<Count1, int>() end
-type Message1() = class inherit Case<Message1, string>() end
+type Flag1()    = class inherit Item<Flag1, bool>() end
+type Count1()   = class inherit Item<Count1, int>() end
+type Message1() = class inherit Item<Message1, string>() end
 
 // NOTE: It happens that in F# is not possible to refer to nested type from the inheritor (the commented code below). 
 // It is much less verbose in C#. The same problem is with `match` clause later in the benchmarks.
 //
-// type FlagOrCount1 = class inherit U<FlagOrCount1, Flag1.data, Count1.data, Message1.data> end
+// type FlagOrCount1 = class inherit Union<FlagOrCount1, Flag1.item, Count1.item, Message1.item> end
 //
-type FlagOrCount1 = class inherit U<FlagOrCount1, Case<Flag1, bool>.data, Case<Count1, int>.data, Case<Message1, string>.data> end
+type FlagOrCount1 = class inherit Union<FlagOrCount1, Item<Flag1, bool>.item, Item<Count1, int>.item, Item<Message1, string>.item> end
 
 [<MemoryDiagnoser>]
 type Traverse_union_array_and_match_to_sum_something() =
@@ -38,8 +46,8 @@ type Traverse_union_array_and_match_to_sum_something() =
         let mutable sum = 0
         for x in _fs do 
             match x with 
-            | Flag f  -> if f then sum <- sum + 1 else sum |> ignore 
-            | Count n -> sum <- sum + n
+            | Flag f    -> if f then sum <- sum + 1 else sum |> ignore 
+            | Count n   -> sum <- sum + n
             | Message _ -> sum <- sum + 42
         sum
 
@@ -49,11 +57,14 @@ type Traverse_union_array_and_match_to_sum_something() =
         for x in _cs1 do
             match x with
             // FlagOrCount1.case1 in C#
-            | :? U<FlagOrCount1, Case<Flag1, bool>.data, Case<Count1, int>.data, Case<Message1, string>.data>.case1 as f -> if f.Case.Value then sum <- sum + 1 else sum |> ignore
+            | :? Union<FlagOrCount1, Item<Flag1, bool>.item, Item<Count1, int>.item, Item<Message1, string>.item>.case1 as f -> 
+                if f.Case.Item then sum <- sum + 1 else sum |> ignore
             // FlagOrCount1.case2 in C#
-            | :? U<FlagOrCount1, Case<Flag1, bool>.data, Case<Count1, int>.data, Case<Message1, string>.data>.case2 as n -> sum <- sum + n.Case.Value
+            | :? Union<FlagOrCount1, Item<Flag1, bool>.item, Item<Count1, int>.item, Item<Message1, string>.item>.case2 as n -> 
+                sum <- sum + n.Case.Item
             // FlagOrCount1.case3 in C#
-            | :? U<FlagOrCount1, Case<Flag1, bool>.data, Case<Count1, int>.data, Case<Message1, string>.data>.case3 -> sum <- sum + 42
+            | :? Union<FlagOrCount1, Item<Flag1, bool>.item, Item<Count1, int>.item, Item<Message1, string>.item>.case3 -> 
+                sum <- sum + 42
             | _ -> ()
         sum
 
@@ -62,9 +73,12 @@ type Traverse_union_array_and_match_to_sum_something() =
         let mutable sum = 0
         for x in _cs1 do
             match x.Tag with
-            | FlagOrCount1.Tag.Case1 -> if (x :?> U<FlagOrCount1, Case<Flag1, bool>.data, Case<Count1, int>.data, Case<Message1, string>.data>.case1).Case.Value then sum <- sum + 1 else sum |> ignore
-            | FlagOrCount1.Tag.Case2 -> sum <- sum + (x :?> U<FlagOrCount1, Case<Flag1, bool>.data, Case<Count1, int>.data, Case<Message1, string>.data>.case2).Case.Value
-            | FlagOrCount1.Tag.Case3 -> sum <- sum + 42
+            | FlagOrCount1.Tag.Case1 -> 
+                if (x :?> Union<FlagOrCount1, Item<Flag1, bool>.item, Item<Count1, int>.item, Item<Message1, string>.item>.case1).Case.Item then sum <- sum + 1 else sum |> ignore
+            | FlagOrCount1.Tag.Case2 -> 
+                sum <- sum + (x :?> Union<FlagOrCount1, Item<Flag1, bool>.item, Item<Count1, int>.item, Item<Message1, string>.item>.case2).Case.Item
+            | FlagOrCount1.Tag.Case3 -> 
+                sum <- sum + 42
             | _ -> ()
         sum
 
@@ -73,9 +87,12 @@ type Traverse_union_array_and_match_to_sum_something() =
         let mutable sum = 0
         for x in _cs1 do
             match x with
-            | :? I<Case<Flag1, bool>.data> as f -> if f.Case.Value then sum <- sum + 1 else sum |> ignore
-            | :? I<Case<Count1, int>.data> as n -> sum <- sum + n.Case.Value
-            | :? I<Case<Message1, string>.data> -> sum <- sum + 42
+            | :? I<Item<Flag1, bool>.item> as f -> 
+                if f.Value.Item then sum <- sum + 1 else sum |> ignore
+            | :? I<Item<Count1, int>.item> as n -> 
+                sum <- sum + n.Value.Item
+            | :? I<Item<Message1, string>.item> -> 
+                sum <- sum + 42
             | _ -> ()
         sum
 
@@ -83,7 +100,7 @@ type Traverse_union_array_and_match_to_sum_something() =
     member this.CSharp_named_case_struct_Match_method() =
         let mutable sum = 0
         for x in _cs1 do 
-            x.Match((fun f -> if f.Value then sum <- sum + 1 else sum |> ignore), (fun n -> sum <- sum + n.Value), (fun m -> sum <- sum + 42)) |> ignore
+            x.Match((fun f -> if f.Item then sum <- sum + 1 else sum |> ignore), (fun n -> sum <- sum + n.Item), (fun m -> sum <- sum + 42)) |> ignore
         sum
 
 
