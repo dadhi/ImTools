246cb1db71dbbeb6bd4f40e595465da767ec24d6
dadhi <max.volkov@gmail.com>
2020-06-12 19:50:06 +0000

    more test cases

--- test/ImTools.UnitTests/Experimental.ImMap234Tests.cs
+++ test/ImTools.UnitTests/Experimental.ImMap234Tests.cs
@@ -13,25 +13,42 @@ namespace ImTools.Experimental.Tree234.UnitTests
             Assert.AreEqual(default(int), m.GetValueOrDefault(13));
 
             m = m.AddOrUpdate(1, 1);
-            Assert.AreEqual(1, m.GetValueOrDefault(1)); 
             Assert.IsInstanceOf<ImMapEntry<int>>(m);
+            Assert.AreEqual(1, m.GetValueOrDefault(1));
 
             m = m.AddOrUpdate(2, 2);
-            Assert.AreEqual(2, m.GetValueOrDefault(2));
             Assert.IsInstanceOf<ImMapLeafs2<int>>(m);
+            Assert.AreEqual(2, m.GetValueOrDefault(2));
 
             m = m.AddOrUpdate(3, 3);
-            Assert.AreEqual(3, m.GetValueOrDefault(3));
             Assert.IsInstanceOf<ImMapLeafs3<int>>(m);
+            Assert.AreEqual(3, m.GetValueOrDefault(3));
 
+            m = m.AddOrUpdate(4, 4);
+            Assert.IsInstanceOf<ImMapBranch2<int>>(m);
+            Assert.IsInstanceOf<ImMapEntry<int>>(((ImMapBranch2<int>)m).Branch0);
+            Assert.IsInstanceOf<ImMapLeafs2<int>>(((ImMapBranch2<int>) m).Branch1);
+            Assert.AreEqual(4, m.GetValueOrDefault(4));
+            Assert.AreEqual(3, m.GetValueOrDefault(3));
+            Assert.AreEqual(2, m.GetValueOrDefault(2));
+            Assert.AreEqual(1, m.GetValueOrDefault(1));
 
+            m = m.AddOrUpdate(5, 5);
+            Assert.IsInstanceOf<ImMapBranch2<int>>(m);
+            Assert.IsNotInstanceOf<ImMapBranch3<int>>(m);
+            Assert.IsInstanceOf<ImMapEntry<int>>(((ImMapBranch2<int>)m).Branch0);
+            Assert.IsInstanceOf<ImMapLeafs3<int>>(((ImMapBranch2<int>)m).Branch1);
+            Assert.AreEqual(5, m.GetValueOrDefault(5));
 
-            //    .AddOrUpdate(2, 2)
-            //    .AddOrUpdate(3, 3);
+            m = m.AddOrUpdate(6, 6);
+            Assert.IsInstanceOf<ImMapBranch3<int>>(m);
+            Assert.IsInstanceOf<ImMapEntry<int>>(((ImMapBranch3<int>)m).Branch0);
+            Assert.IsInstanceOf<ImMapEntry<int>>(((ImMapBranch3<int>)m).Branch1);
+            Assert.IsInstanceOf<ImMapLeafs2<int>>(((ImMapBranch3<int>)m).Branch2);
+            Assert.AreEqual(3, m.GetValueOrDefault(3));
+            Assert.AreEqual(5, m.GetValueOrDefault(5));
+            Assert.AreEqual(6, m.GetValueOrDefault(6));
 
-            //Assert.AreEqual(11, t.GetValueOrDefault(1));
-            //Assert.AreEqual(22, t.GetValueOrDefault(2));
-            //Assert.AreEqual(33, t.GetValueOrDefault(3));
         }
     }
 }
