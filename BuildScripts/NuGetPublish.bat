758a63f9ee354daca8d419843934bc6c866b6b58
dadhi <max.volkov@gmail.com>
2021-05-10 22:32:18 +0300

    updating source code package

--- BuildScripts/NuGetPublish.bat
+++ BuildScripts/NuGetPublish.bat
@@ -4,8 +4,8 @@ set PACKAGES=..\.dist\packages
 set SOURCE=https://api.nuget.org/v3/index.json
 set /p APIKEY=<"..\ApiKey.txt"
 
-rem dotnet nuget push "%PACKAGES%\ImTools.dll.2.0.0.nupkg" -k %APIKEY% -s %SOURCE%
-rem dotnet nuget push "%PACKAGES%\ImTools.2.0.0.nupkg" -k %APIKEY% -s %SOURCE%
+dotnet nuget push "%PACKAGES%\ImTools.dll.3.1.0.nupkg" -k %APIKEY% -s %SOURCE%
+dotnet nuget push "%PACKAGES%\ImTools.3.1.0.nupkg" -k %APIKEY% -s %SOURCE%
 
 echo:
 echo:Publishing completed.
