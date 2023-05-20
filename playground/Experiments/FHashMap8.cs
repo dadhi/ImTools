2371d0dee0d1ff3ed6a5ec24e31c4fa81e96ef3a
dadhi <max.volkov@gmail.com>
2023-05-20 18:26:33 +0200

    FHashMap8 with 2 int hashes in a long

---
+++ playground/Experiments/FHashMap8.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace ImTools.Experiments;
+
+public static class FHashMap8Extensions
+{
+    public static Item<K, V>[] Explain<K, V, TEq>(this FHashMap8<K, V, TEq> map) where TEq : struct, IEqualityComparer<K>
+    {
+#if DEBUG
+        Debug.WriteLine($"FirstProbeAdditions: {map.FirstProbeAdditions}, MaxProbes: {map.MaxProbes}");
+#endif
+
+        var entries = map._entries;
+        var hashesAndIndexes = map._hashesAndIndexes;
+        var capacity = hashesAndIndexes.Length;
+        var indexMask = map._indexMask;
+
+        var items = new Item<K, V>[hashesAndIndexes.Length];
+
+        for (var i = 0; i < hashesAndIndexes.Length; i++)
+        {
+            var h = hashesAndIndexes[i];
+            if (h == 0)
+                continue;
+
+            var probe = (byte)(h >> FHashMap8<K, V, TEq>.ProbeCountShift);
+            var hashIndex = (capacity + i - (probe - 1)) & indexMask;
+
+            var hashMiddle = (h & FHashMap8<K, V, TEq>.HashAndIndexMask & ~indexMask);
+            var hash = hashMiddle | hashIndex;
+            var index = h & indexMask;
+
+            string hkv = null;
+            var heq = false;
+            if (probe != 0)
+            {
+                var e = entries[index];
+                var kh = e.Key.GetHashCode() & FHashMap8<K, V, TEq>.HashAndIndexMask;
+                heq = kh == hash;
+                hkv = $"{kh.b()}:{e.Key}->{e.Value}";
+            }
+            items[i] = new Item<K, V> { Probe = probe, Hash = hash.b(), HEq = heq, Index = index, HKV = hkv };
+        }
+        return items;
+    }
+
+    public struct Item<K, V>
+    {
+        public byte Probe;
+        public bool HEq;
+        public string Hash;
+        public string HKV;
+        public int Index;
+        public bool IsEmpty => Probe == 0;
+        public string Output => $"{Probe}|{Hash}{(HEq ? "==" : "!=")}{HKV}";
+        public override string ToString() => IsEmpty ? "empty" : Output;
+    }
+}
+
+#if DEBUG
+public class FHashMap8DebugProxy<K, V, TEq> where TEq : struct, IEqualityComparer<K>
+{
+    private readonly FHashMap8<K, V, TEq> _map;
+    public FHashMap8DebugProxy(FHashMap8<K, V, TEq> map) => _map = map;
+    [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+    public FHashMap8Extensions.Item<K, V>[] Items => _map.Explain();
+}
+
+[DebuggerTypeProxy(typeof(FHashMap8DebugProxy<,,>))]
+[DebuggerDisplay("Count={Count}")]
+#endif
+public sealed class FHashMap8<K, V, TEq> where TEq : struct, IEqualityComparer<K>
+{
+    // todo: @improve make the Entry a type parameter to map and define TEq in terms of the Entry, 
+    // todo: @improve it will allow to introduce the Set later without the Value in the Entry, end the Entry may be the Key itself
+    [DebuggerDisplay("{Key}->{Value}")]
+    public struct Entry
+    {
+        public K Key;
+        public V Value;
+    }
+
+    public const int DefaultSeedCapacity = 16;
+    public const byte MinFreeCapacityShift = 3; // e.g. for the DefaultCapacity 16 >> 3 => 2, so 2 free slots is 12.5% of the capacity  
+
+    public const byte MaxProbeBits = 5; // 5 bits max
+    public const byte MaxProbeCount = (1 << MaxProbeBits) - 1;
+    public const byte ProbeCountShift = 32 - MaxProbeBits;
+    public const int SingleProbeMask = 1 << ProbeCountShift;
+    public const int HashAndIndexMask = ~(MaxProbeCount << ProbeCountShift);
+
+    // todo: @perf what if use the long to store a pair if entries and batch work on them in pairs?
+    // The _hashesAndIndexes item is the Int32, 
+    // e.g. 00010|000...110|01101
+    //      |     |         |- The index into the _entries array, 0-based. It is the size of the hashes array size-1 (e.g. 15 for the 16). 
+    //      |     |         | This part of the erased hash is used to get the ideal index into the hashes array, so we are safely using it to store the index into entries.
+    //      |     |- The middle bits of the hash
+    //      |- 5 high bits of the Probe count, with the minimal value of 00001  indicating non-empty slot.
+    // todo: @add For the removed hash we won't use the tumbstone but will actually remove the hash.
+    internal int[] _hashesAndIndexes;
+    internal Entry[] _entries;
+    internal int _indexMask; // pre-calculated and saved on the DoubleSize for the performance
+    internal int _entryCount;
+
+    public int[] HashesAndIndexes => _hashesAndIndexes;
+    public Entry[] Entries => _entries;
+    public int Count => _entryCount;
+
+    public FHashMap8(int seedCapacity = DefaultSeedCapacity)
+    {
+        // double the size of the hashes, because they are cheap, 
+        // this will also provide the flexibility of independence of the sizes of hashes and entries
+        _hashesAndIndexes = new int[seedCapacity << 1];
+        _entries = new Entry[seedCapacity];
+        _indexMask = (seedCapacity << 1) - 1;
+        _entryCount = 0;
+
+        // todo: @perf benchmark the un-initialized array?
+        // _entries = GC.AllocateUninitializedArray<Entry>(capacity); // todo: create without default values using via GC.AllocateUninitializedArray<Entry>(size);
+    }
+
+    [MethodImpl((MethodImplOptions)256)] // MethodImplOptions.AggressiveInlining
+    public bool TryGetValue(K key, out V value)
+    {
+        var hash = default(TEq).GetHashCode(key);
+
+#if NET7_0_OR_GREATER
+        ref var hashesAndIndexes = ref System.Runtime.InteropServices.MemoryMarshal.GetArrayDataReference(_hashesAndIndexes);
+#else
+        var hashesAndIndexes = _hashesAndIndexes;
+#endif
+        var indexMask = _indexMask;
+        var probeAndHashMask = ~indexMask;
+
+        var hashMiddle = hash & ~indexMask & HashAndIndexMask;
+        var hashIndex = hash & indexMask;
+
+        byte probes = 1;
+        while (true)
+        {
+#if NET7_0_OR_GREATER
+            var h = Unsafe.Add(ref hashesAndIndexes, hashIndex);
+#else
+            var h = hashesAndIndexes[hashIndex];
+#endif
+            // the check is the first because if look fo the present key, 
+            // we will avoid the unnecessary exit condition below. 
+            // refarding the check for missing key, it is fine to pain one comparison, 
+            // imho - you  will need to select what you care for ;)
+            if ((h & probeAndHashMask) == ((probes << ProbeCountShift) | hashMiddle))
+            {
+                ref var e = ref _entries[h & indexMask];
+                if (default(TEq).Equals(e.Key, key))
+                {
+                    value = e.Value;
+                    return true;
+                }
+            }
+            
+            if ((h >> ProbeCountShift) < probes)
+                break;
+            
+            hashIndex = (hashIndex + 1) & indexMask;
+            ++probes;
+        }
+        value = default;
+        return false;
+    }
+
+    public V GetValueOrDefault(K key, V defaultValue = default)
+    {
+        var hash = default(TEq).GetHashCode(key);
+
+#if NET7_0_OR_GREATER
+        ref var hashesAndIndexes = ref System.Runtime.InteropServices.MemoryMarshal.GetArrayDataReference(_hashesAndIndexes);
+#else
+        var hashesAndIndexes = _hashesAndIndexes;
+#endif
+        var indexMask = _indexMask;
+        var probeAndHashMask = ~indexMask;
+
+        var hashMiddle = hash & ~indexMask & HashAndIndexMask;
+        var hashIndex = hash & indexMask;
+
+        byte probes = 1;
+        while (true)
+        {
+#if NET7_0_OR_GREATER
+            var h = Unsafe.Add(ref hashesAndIndexes, hashIndex);
+#else
+            var h = hashesAndIndexes[hashIndex];
+#endif
+            if ((h & probeAndHashMask) == ((probes << ProbeCountShift) | hashMiddle))
+            {
+                ref var e = ref _entries[h & indexMask]; // todo: @perf wrap access into the interface to separate the entries abstraction
+                if (default(TEq).Equals(e.Key, key))
+                    return e.Value;
+            }
+            if ((h >> ProbeCountShift) < probes)
+                break;
+            hashIndex = (hashIndex + 1) & indexMask;
+            ++probes;
+        }
+        return default;
+    }
+
+#if DEBUG
+    public int MaxProbes = 1;
+    public int FirstProbeAdditions = 0;
+#endif
+
+    // todo: @perf consider using GetArrayDataReference the same as Lookup methods
+    public void AddOrUpdate(K key, V value)
+    {
+        var hash = default(TEq).GetHashCode(key);
+
+        var hashesAndIndexes = _hashesAndIndexes;
+        var indexMask = _indexMask;
+        if (indexMask - _entryCount <= (indexMask >> MinFreeCapacityShift)) // if the free capacity is less free slots 1/16 (6.25%)
+        {
+            _hashesAndIndexes = hashesAndIndexes = DoubleSize(_hashesAndIndexes);
+            _indexMask = indexMask = (indexMask << 1) | 1;
+#if DEBUG
+            Debug.WriteLine($"Resize _hashesAndIndexes to {_indexMask + 1} because the _entryCount:{_entryCount}");
+#endif
+        }
+
+        var hashIndex = hash & indexMask;
+        var hashMiddle = hash & ~indexMask & HashAndIndexMask;
+        var hashAndEntryIndex = 0;
+        var h = 0;
+        byte probes = 1;
+        for (; ; ++probes, hashIndex = (hashIndex + 1) & indexMask)
+        {
+            Debug.Assert(probes <= MaxProbeCount, $"DEBUG ASSERT FAILED probes:{probes} <= MaxProbeCount:{MaxProbeCount}");
+
+            hashAndEntryIndex = (probes << ProbeCountShift) | hashMiddle;
+            h = hashesAndIndexes[hashIndex];
+            if (h == 0)
+            {
+#if DEBUG
+                if (probes == 1)
+                    ++FirstProbeAdditions;
+                if (probes > MaxProbes)
+                {
+                    MaxProbes = probes;
+                    Debug.WriteLine($"NEW: MaxProbes increased to {probes} when adding key:`{key}`");
+                }
+#endif   
+                var entryCount = _entryCount;
+                hashesAndIndexes[hashIndex] = hashAndEntryIndex | entryCount;
+
+                // todo: @wip wrap in the abstraction together with the check and Resize, 
+                // because the abstraction may decide to avoid it completely, e.g. by pre-allocation of enough entries
+                if (entryCount + 1 >= _entries.Length)
+                    Array.Resize(ref _entries, _entries.Length << 1);
+                ref var e = ref _entries[entryCount];
+                e.Key = key;
+                e.Value = value;
+
+                _entryCount = entryCount + 1;
+                return;
+            }
+
+            // the check is here, because it is cheaper than hash comparison below and current ont may result in early break/exit
+            var hp = h >> ProbeCountShift;
+            if (hp < probes) // skip hashes with the bigger probe count until we find the same or less probes
+            {
+                // Robin Hood goes here to steal from the rich (with the less probe count) and give to the poor (with more probes).
+                var entryCount = _entryCount;
+                hashesAndIndexes[hashIndex] = hashAndEntryIndex | entryCount;
+                hashAndEntryIndex = h & HashAndIndexMask;
+                probes = (byte)hp;
+
+                if (entryCount + 1 >= _entries.Length)
+                    Array.Resize(ref _entries, _entries.Length << 1);
+                ref var e = ref _entries[entryCount];
+                e.Key = key;
+                e.Value = value;
+
+                _entryCount = entryCount + 1;
+                break;
+            }
+
+            if ((h & ~indexMask) == hashAndEntryIndex)
+            {
+#if DEBUG
+                Debug.WriteLine($"NEW: hp < probes `{probes}` and hashes are equal for the added key:`{key}` and existing key:`{_entries[h & indexMask].Key}`");
+#endif
+                ref var e = ref _entries[h & indexMask]; // check the existing entry key and update the value if the keys are matched
+                if (default(TEq).Equals(e.Key, key))
+                {
+                    e.Value = value;
+                    return;
+                }
+            }
+        }
+
+        // todo: @simplify factor out into the method X
+        while (true)
+        {
+            ++probes;
+            hashIndex = (hashIndex + 1) & indexMask;
+            h = hashesAndIndexes[hashIndex];
+            if (h == 0)
+            {
+                hashesAndIndexes[hashIndex] = (probes << ProbeCountShift) | hashAndEntryIndex;
+                return;
+            }
+            var hp = h >> ProbeCountShift;
+            if (hp < probes) // skip hashes with the bigger probe count until we find the same or less probes
+            {
+                hashesAndIndexes[hashIndex] = (probes << ProbeCountShift) | hashAndEntryIndex;
+                hashAndEntryIndex = h & HashAndIndexMask;
+                probes = (byte)hp;
+            }
+        }
+    }
+
+    // todo: @perf pass the oldIndexMask, so we will calculate the oldCapacity out of it
+    internal static int[] DoubleSize(int[] oldHashesAndIndexes)
+    {
+        var oldCapacity = oldHashesAndIndexes.Length;
+        var newCapacity = oldCapacity << 1;
+        var newHashesAndIndexes = new int[newCapacity];
+
+        var oldIndexMask = oldCapacity - 1;
+        var newIndexMask = newCapacity - 1;
+        var newHashMiddleMask = ~newIndexMask & HashAndIndexMask;
+
+        // todo: @perf find the way to avoid copying the hashes with 0 next bit and with ideal+ probe count
+        for (var i = 0; i < (uint)oldHashesAndIndexes.Length; i++)
+        {
+            var oldHash = oldHashesAndIndexes[i];
+            if (oldHash == 0)
+                continue;
+
+            // get the new hash index for the new capacity by restoring the (possibly wrapped) 
+            // probes count (and therefore the distance from the ideal hash position) 
+            var distance = (oldHash >> ProbeCountShift) - 1;
+            var oldHashIndex = (oldCapacity + i - distance) & oldIndexMask;
+            var restoredOldHash = (oldHash & ~oldIndexMask) | oldHashIndex;
+            var newHashIndex = restoredOldHash & newIndexMask;
+
+            // erasing the next to capacity bit, given the capacity was 4 and now it is 4 << 1 = 8, 
+            // we are erasing the 3rd bit to store the new count in it. 
+            var newHashAndEntryIndex = oldHash & HashAndIndexMask & ~oldCapacity;
+
+            var h = 0;
+            // todo: @simplify factor out into the method X
+            for (byte probes = 1; ; ++probes, newHashIndex = (newHashIndex + 1) & newIndexMask) // we don't need the condition for the MaxProbes because by increasing the hash space we guarantee that we fit the hashes in the finite amount of probes likely less than previous MaxProbeCount
+            {
+                h = newHashesAndIndexes[newHashIndex];
+                if (h == 0)
+                {
+                    newHashesAndIndexes[newHashIndex] = (probes << ProbeCountShift) | newHashAndEntryIndex;
+                    break;
+                }
+                var hp = h >> ProbeCountShift;
+                if (hp < probes)
+                {
+                    newHashesAndIndexes[newHashIndex] = (probes << ProbeCountShift) | newHashAndEntryIndex;
+                    newHashAndEntryIndex = h & HashAndIndexMask;
+                    probes = (byte)hp; // todo: @perf Unsafe.As<int, byte>(ref hp)?
+                }
+            }
+        }
+#if DEBUG
+        // this will output somthing like this for capacity 32:
+        // -_-112--___12-3--44452-223-42311
+        // todo: @perf can we move the non`-` hashes in a one loop if possible or non move at all? 
+        Debug.Write("before resize:");
+        foreach (var it in oldHashesAndIndexes)
+            Debug.Write(it == 0 ? "_"
+            : (it & oldCapacity) != 0 ? "-"
+            : (it >> ProbeCountShift).ToString());
+
+        Debug.WriteLine("");
+#endif
+        return newHashesAndIndexes;
+    }
+}
---
+++ test/ImTools.UnitTests/Playground/FHashMap8Tests.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace ImTools.Experiments.UnitTests;
+
+[TestFixture]
+public class FHashMap8Tests
+{
+    public static void Verify<K, V, TEq>(FHashMap8<K, V, TEq> map) where TEq : struct, IEqualityComparer<K>
+    {
+        var exp = map.Explain();
+        foreach (var it in exp)
+            if (!it.IsEmpty)
+                Assert.True(it.HEq);
+
+        // Verify the indexes
+        var uniq = new Dictionary<K, int>(map.Count);
+        var entryIndexMask = map.HashesAndIndexes.Length - 1;
+        var entries = map.Entries;
+        foreach (var it in map.HashesAndIndexes)
+            if (it != 0)
+            {
+                var entryIndex = it & entryIndexMask;
+                var key = entries[entryIndex].Key;
+                if (!uniq.TryGetValue(key, out var count))
+                    uniq.Add(key, 1);
+                else
+                {
+                    Assert.Fail($"Duplicate key: {key}");
+                    uniq[key] = count + 1;
+                }
+            }
+    }
+
+    [Test]
+    public void Real_world_test_AddOrUpdate()
+    {
+        var types = typeof(Dictionary<,>).Assembly.GetTypes().Take(100).ToArray();
+
+        var map = new ImTools.Experiments.FHashMap8<Type, string, RefEq<Type>>();
+
+        foreach (var key in types)
+            map.AddOrUpdate(key, "a");
+
+        map.AddOrUpdate(typeof(Console), "!");
+
+        Assert.AreEqual(101, map.Count);
+
+        Verify(map);
+    }
+
+    [Test]
+    public void Real_world_test_TryGetValue()
+    {
+        var types = typeof(Dictionary<,>).Assembly.GetTypes().Take(100).ToArray();
+
+        var map = new ImTools.Experiments.FHashMap8<Type, string, RefEq<Type>>();
+
+        foreach (var key in types)
+            map.AddOrUpdate(key, "a");
+
+        map.AddOrUpdate(typeof(Console), "!");
+
+        var found = map.TryGetValue(typeof(Console), out var value);
+        Assert.IsTrue(found);
+        Assert.AreEqual("!", value);
+
+        Verify(map);
+    }
+
+    [Test]
+    public void Real_world_test_without_Resize()
+    {
+        var types = typeof(Dictionary<,>).Assembly.GetTypes().Take(100).ToArray();
+
+        var map = new ImTools.Experiments.FHashMap8<Type, string, RefEq<Type>>(128);
+
+        foreach (var key in types)
+            map.AddOrUpdate(key, "a");
+
+        map.AddOrUpdate(typeof(FHashMap8Tests), "!");
+
+        Assert.AreEqual(101, map.Count);
+
+        Verify(map);
+    }
+
+    [Test]
+    public void Can_store_and_retrieve_value_from_map()
+    {
+        var map = new FHashMap8<int, string, IntEq>();
+
+        map.AddOrUpdate(42, "1");
+        map.AddOrUpdate(42 + 32, "2");
+        map.AddOrUpdate(42 + 32 + 32, "3");
+
+        // interrupt the keys with ne key
+        map.AddOrUpdate(43, "a");
+        map.AddOrUpdate(43 + 32, "b");
+        map.AddOrUpdate(43 + 32 + 32, "c");
+
+        map.AddOrUpdate(42 + 32 + 32 + 32, "4");
+
+        // insert 3rd variety of the keys
+        map.AddOrUpdate(44, "*");
+
+        map.AddOrUpdate(42 + 32 + 32 + 32 + 32, "5");
+        map.AddOrUpdate(42 + 32 + 32 + 32 + 32 + 32, "6");
+        map.AddOrUpdate(43 + 32 + 32 + 32, "d");
+
+        map.AddOrUpdate(42 + 32 + 32 + 32 + 32 + 32 + 32, "7");
+        map.AddOrUpdate(42 + 32 + 32 + 32 + 32 + 32 + 32 + 32, "8");
+
+        // check for the missing key
+        Assert.AreEqual(null, map.GetValueOrDefault(43 + 32 + 32 + 32 + 32));
+
+        // check for the strange key
+        Assert.AreEqual("*", map.GetValueOrDefault(44));
+
+        Assert.AreEqual("1", map.GetValueOrDefault(42));
+        Assert.AreEqual("2", map.GetValueOrDefault(42 + 32));
+        Assert.AreEqual("3", map.GetValueOrDefault(42 + 32 + 32));
+        Assert.AreEqual("4", map.GetValueOrDefault(42 + 32 + 32 + 32));
+        Assert.AreEqual("5", map.GetValueOrDefault(42 + 32 + 32 + 32 + 32));
+        Assert.AreEqual("6", map.GetValueOrDefault(42 + 32 + 32 + 32 + 32 + 32));
+        Assert.AreEqual("7", map.GetValueOrDefault(42 + 32 + 32 + 32 + 32 + 32 + 32));
+        Assert.AreEqual("8", map.GetValueOrDefault(42 + 32 + 32 + 32 + 32 + 32 + 32 + 32));
+
+        Assert.AreEqual("a", map.GetValueOrDefault(43));
+        Assert.AreEqual("b", map.GetValueOrDefault(43 + 32));
+        Assert.AreEqual("c", map.GetValueOrDefault(43 + 32 + 32));
+        Assert.AreEqual("d", map.GetValueOrDefault(43 + 32 + 32 + 32));
+
+        Assert.AreEqual(13, map.Count);
+
+        Verify(map);
+    }
+
+    [Test]
+    public void Can_store_and_retrieve_value_from_map_with_Expand_in_the_middle()
+    {
+        var map = new FHashMap8<int, string, IntEq>(2);
+
+        map.AddOrUpdate(42, "1");
+        map.AddOrUpdate(42 + 32, "2");
+
+        // interrupt the keys with ne key
+        map.AddOrUpdate(43, "a");
+        map.AddOrUpdate(43 + 32, "b");
+
+        map.AddOrUpdate(42 + 32 + 32, "3");
+
+        Assert.AreEqual("1", map.GetValueOrDefault(42));
+        Assert.AreEqual("2", map.GetValueOrDefault(42 + 32));
+        Assert.AreEqual("3", map.GetValueOrDefault(42 + 32 + 32));
+        Assert.AreEqual(null, map.GetValueOrDefault(42 + 32 + 32 + 32));
+        Assert.AreEqual("a", map.GetValueOrDefault(43));
+
+        map.AddOrUpdate(43, "a!");
+        Assert.AreEqual("a!", map.GetValueOrDefault(43));
+
+        Verify(map);
+    }
+
+    [Test]
+    public void Can_store_and_get_stored_item_count()
+    {
+        var map = new FHashMap8<int, string, IntEq>();
+
+        map.AddOrUpdate(42, "1");
+        map.AddOrUpdate(42 + 32 + 32, "3");
+
+        Assert.AreEqual(2, map.Count);
+        Verify(map);
+    }
+
+    [Test]
+    public void Can_update_a_stored_item_with_new_value()
+    {
+        var map = new FHashMap8<int, string, IntEq>();
+
+        map.AddOrUpdate(42, "1");
+        map.AddOrUpdate(42, "3");
+
+        Assert.AreEqual("3", map.GetValueOrDefault(42));
+        Assert.AreEqual(1, map.Count);
+        Verify(map);
+    }
+
+    [Test]
+    public void Can_add_key_with_0_hash_code()
+    {
+        var map = new FHashMap8<int, string, IntEq>();
+
+        map.AddOrUpdate(0, "aaa");
+        map.AddOrUpdate(0 + 32, "2");
+        map.AddOrUpdate(0 + 32 + 32, "3");
+        Verify(map);
+
+        string value;
+        Assert.IsTrue(map.TryGetValue(0, out value));
+
+        Assert.AreEqual("aaa", value);
+    }
+
+    [Test]
+    public void Can_quickly_find_the_scattered_items_with_the_same_cache()
+    {
+        var map = new FHashMap8<int, string, IntEq>();
+
+        map.AddOrUpdate(42, "1");
+        map.AddOrUpdate(43, "a");
+        map.AddOrUpdate(42 + 32, "2");
+        map.AddOrUpdate(45, "b");
+        map.AddOrUpdate(46, "c");
+        map.AddOrUpdate(42 + 32 + 32, "3");
+        Verify(map);
+
+        string value;
+        Assert.IsTrue(map.TryGetValue(42 + 32, out value));
+        Assert.AreEqual("2", value);
+
+        Assert.IsTrue(map.TryGetValue(42 + 32 + 32, out value));
+        Assert.AreEqual("3", value);
+    }
+
+    // [Test]
+    // public void Can_remove_the_stored_item()
+    // {
+    //     var map = new FHashMap<int, string>();
+
+    //     map.AddOrUpdate(42, "1");
+    //     map.AddOrUpdate(42 + 32, "2");
+    //     map.AddOrUpdate(42 + 32 + 32, "3");
+
+    //     map.Remove(42 + 32);
+
+    //     Assert.AreEqual(2, map.Count);
+    // }
+}
