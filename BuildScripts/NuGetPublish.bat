b952e51c204a2c75e8ce32880ae632b8c0660ba0
dadhi <max.volkov@gmail.com>
2024-05-23 14:17:08 +0200

    adding readme to the package and other stuff

--- BuildScripts/NuGetPublish.bat
+++ BuildScripts/NuGetPublish.bat
@@ -4,8 +4,8 @@ set PACKAGES=.dist\packages
 set SOURCE=https://api.nuget.org/v3/index.json
 set /p APIKEY=<"..\ApiKey.txt"
 
-dotnet nuget push "%PACKAGES%\ImTools.dll.4.0.0.nupkg" -k %APIKEY% -s %SOURCE%
-dotnet nuget push "%PACKAGES%\ImTools.4.0.0.nupkg" -k %APIKEY% -s %SOURCE%
+dotnet nuget push "%PACKAGES%\ImTools.dll.5.0.0-preview-01.nupkg" -k %APIKEY% -s %SOURCE%
+dotnet nuget push "%PACKAGES%\ImTools.dll.5.0.0-preview-01.snupkg" -k %APIKEY% -s %SOURCE%
 
 echo:
 echo:Publishing completed.
